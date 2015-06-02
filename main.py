347942ffd96a798addd28eb7f84c67f34737479e
Karel Mácha <karel.macha@karlitos.net>
2015-06-03 01:07:00 +0200

    Adds a little better implementation of the multiple screens with the Post wall and the Post carousel widgets. The movevemnt with the arrow keys works, switching between the screens also works, quitting the application works. The browsing of the posts is not synchronous, the application does not globbaly track which post is currently active.

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ class PostWall(RecycleView):
         self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
         if not self._keyboard:
             return
-        self._keyboard.bind(on_key_down=self.on_keyboard_down)
+        #self._keyboard.bind(on_key_down=self.on_keyboard_down)
 
         self.movement_flag = False
 
@@ -53,41 +53,36 @@ class PostWall(RecycleView):
         def animation_complete(animation, widget):
             self.movement_flag = False
 
-        key = keycode[1]
-        print 'keyboard', key, text
+    def move_wall(self, direction):
+        def animation_complete(animation, widget):
+            self.movement_flag = False
 
-        if (key == 'up' or key == 'down') and not self.movement_flag:
-            computed_positions = self.current_layout_manager.computed_positions
-            if (key == 'down' and self.selected_post_index >= len(computed_positions) - 1) or\
-                    (key == 'up' and self.selected_post_index <= 0):
-                return False
-            # the layout height ist the last "position"
-            computed_positions = computed_positions + [self.ids.layout.height]
-            scrollview = self.ids.sv
+        computed_positions = self.current_layout_manager.computed_positions
+        if (direction == 'down' and self.selected_post_index >= len(computed_positions) - 1) or\
+                (direction == 'up' and self.selected_post_index <= 0):
+            return False
+        # the layout height ist the last "position"
+        computed_positions = computed_positions + [self.ids.layout.height]
+        scrollview = self.ids.sv
 
-            next_index = self.selected_post_index + 1 if key == 'down' else self.selected_post_index - 1
-            scroll_dist = self.compute_vertical_scroll_distance(scrollview, computed_positions, next_index)
+        next_index = self.selected_post_index + 1 if direction == 'down' else self.selected_post_index - 1
+        scroll_dist = self.compute_vertical_scroll_distance(scrollview, computed_positions, next_index)
 
-            # do the animation
-            self.movement_flag = True
+        # do the animation
+        self.movement_flag = True
 
-            animation = Animation(scroll_y=scroll_dist,  duration=.15)
-            animation.bind(on_complete=animation_complete)
-            animation.start(scrollview)
-            #scrollview._update_effect_y_bounds()
-            previous_selected_widget = self.current_adapter.views[self.selected_post_index]
-            animation = Animation(background_color=previous_selected_widget.default_background_color,  duration=.2)
-            animation.start(previous_selected_widget)
-            next_selected_widget = self.current_adapter.views[next_index]
-            animation = Animation(background_color=[0, 0, .5, .7],  duration=.2)
-            animation.start(next_selected_widget)
-            self.selected_post_index = next_index
-        elif key == 'enter':
-            # usage of an id will be wiser
-            self.parent.parent.current = 'Post carousel'
-        elif key == 'q':
-            App.get_running_app().stop()
-            return True
+        animation = Animation(scroll_y=scroll_dist,  duration=.15)
+        animation.bind(on_complete=animation_complete)
+        animation.start(scrollview)
+        #scrollview._update_effect_y_bounds()
+        previous_selected_widget = self.current_adapter.views[self.selected_post_index]
+        animation = Animation(background_color=previous_selected_widget.default_background_color,  duration=.2)
+        animation.start(previous_selected_widget)
+        next_selected_widget = self.current_adapter.views[next_index]
+        animation = Animation(background_color=[0, 0, .5, .7],  duration=.2)
+        animation.start(next_selected_widget)
+        self.selected_post_index = next_index
+
 
     def compute_vertical_scroll_distance(self, scrollview, computed_positions, post_index):
         # compute the relative distance
@@ -117,52 +112,76 @@ class PostWall(RecycleView):
 class PostCarousel(Carousel):
     def __init__(self, **kwargs):
         super(PostCarousel, self).__init__(**kwargs)
+        self.loop = False
+
+
+class PostScreenManager(ScreenManager):
+    def __init__(self, **kwargs):
+        super(PostScreenManager, self).__init__(**kwargs)
+        self.transition = RiseInTransition()
+
+        # get the post wall and post carousel references
+        wall = self.ids['pw']
+        carousel = self.ids['pc']
+
+         # generate new data
+        data = post_generator.generate_new_data()
+        # pass the data tot he post wall
+
+        wall.data = data
+        # feed the carousel with the data
+        for item in data:
+            if 'ImagePost' in item['viewclass']:
+                carousel.add_widget(Image(source=item['image_path']))
+            elif 'TextPost' in item['viewclass']:
+                carousel.add_widget(Label(text=item['label_text']))
 
         self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
         if not self._keyboard:
             return
         self._keyboard.bind(on_key_down=self.on_keyboard_down)
 
+        self.movement_flag = False
+        Clock.schedule_once(wall.center_first_post, 0.2)
+
     def _keyboard_closed(self):
         self._keyboard.unbind(on_key_down=self.on_keyboard_down)
         self._keyboard = None
 
+        self.movement_flag = False
+
     def on_keyboard_down(self, keyboard, keycode, text, modifiers):
+        def animation_complete(animation, widget):
+            self.movement_flag = False
 
         key = keycode[1]
-        print 'keyboard 2', key, text
-
-        if key == 'up':
-            self.load_previous()
-        elif key == 'down':
-            self.load_next()
-
+        print 'keyboard screen manager', key, text
+        # general key bindings
+        if key == 'q':
+            App.get_running_app().stop()
+            return True
+        # key binding for the wall
+        elif self.current == 'Wall screen':
+            if (key == 'up' or key == 'down') and not self.movement_flag:
+                self.ids['pw'].move_wall(key)
+                return True
+            elif key == 'enter':
+                self.current = 'Carousel screen'
+                return True
+        # key bindings for the carousel
+        elif self.current == 'Carousel screen':
+            if key == 'up':
+                self.ids['pc'].load_previous()
+            elif key == 'down':
+                self.ids['pc'].load_next()
+            elif key == 'backspace':
+                self.current = 'Wall screen'
+                return True
 
 
 class WallApp(App):
 
     def build(self):
-        #root_widget = RootWidget()
-        # Create the manager
-        sm = ScreenManager(transition=RiseInTransition())
-        wall = PostWall()
-        wall_screen = Screen(name='Post wall')
-        wall_screen.add_widget(wall)
-        sm.add_widget(wall_screen)
-        carousel = PostCarousel(loop='false')
-        carousel_screen = Screen(name='Post carousel')
-        carousel_screen.add_widget(carousel)
-        sm.add_widget(carousel_screen)
-        # generate new data
-        data = post_generator.generate_new_data()
-        wall.data = data
-        for item in data:
-            if 'ImagePost' in item['viewclass']:
-                carousel.add_widget(Image(source=item['image_path']))
-            elif 'TextPost' in item['viewclass']:
-                carousel.add_widget(Label(text=item['label_text']))
-        Clock.schedule_once(wall.center_first_post, 0.2)
-       # Clock.schedule_once(self.change_to_carousel, 5)
-        return sm
+        return PostScreenManager()
 
 WallApp().run()
